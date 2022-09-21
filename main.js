f6f42bc2be50a975d5d0077306748ec371e3b348
NiniAlejandraRiosArdilaADSO <ninialejandra0424@gmail.com>
2022-09-21 11:52:13 -0500

    EJERCICIO 2/GUIA2

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 addEventListener(`DOMContentLoaded`, ()=>{
-    //   Se requiere calcular la distancia entre dos puntos, realizar el código que permita hallar la solución, por favor utilizar funciones matemáticas de Python para ello 
+    //   Pablo compro 5 productos los dos primeros con un 5% de descuento y los 2 últimos con un 2% de  descuento, realice un algoritmo que indique cuanto fue el valor a pagar por cada producto y el total  de la compra 
 
-    let x1 =Number(prompt(`x1 :  `, 4));
-    let x2 =Number(prompt(`x2 :  `, 5));
-    let y1 =Number(prompt(`y1 :  `, 6));
-    let y2 =Number(prompt(`y2 :  `, 7));
-    let distancia = ((x2 - x1) * (x2-x1) + (y2-y1) * (y2-y1))
+    let c1 =Number(prompt(`x1 :  `, 4));
+    let c2 =Number(prompt(`x2 :  `, 5));
+    let c3 =Number(prompt(`y1 :  `, 6));
+    let c4 =Number(prompt(`y2 :  `, 7));
+    let c5 = ((x2 - x1) * (x2-x1) + (y2-y1) * (y2-y1))
  
     console.log (`VALOR DE DISTANCIA  : ${distancia} `)
 
